ac9d9e4b75abe91d1f5476154756b1981a30eb87
calmCoder21 <natnaelkassahun13@gmail.com>
2026-04-20 00:16:13 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -67,7 +67,7 @@ export default function LoginPage() {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://abissinia-school.vercel.app/auth/callback",
+        redirectTo: "https://abissinia-school.vercel.app/auth/callback",
       },
     });
 
